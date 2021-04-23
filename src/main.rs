94c2c1d2a8c4d8a1b4f64f42b344320c3524041d
Pratik Gorade <pratikgorade0@gmail.com>
2021-04-23 22:01:58 +0530

    keep trying till you get 6-digit captcha
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -12,23 +12,27 @@ struct CaptchaData {
 async fn get_erp_data() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
-    let r = Request::builder()
-        .method(Method::POST)
-        .uri("https://erp.mitwpu.edu.in/AdminLogin.aspx/funGenerateCaptcha")
-        .header("Content-Type", "application/json; charset=utf-8")
-        .header("Content-Length", "0")
-        .body(Body::empty())
-        .unwrap();
-    let resp = client.request(r).await?;
-    let data = body::to_bytes(resp).await?;
-    let captcha_data: CaptchaData = serde_json::from_slice(&data).unwrap();
-    let image_vector = base64::decode(captcha_data.d).unwrap();
-    // let filename = format!("{}.png", Uuid::new_v4().to_string());
-    // std::fs::write(&filename, &image_vector).unwrap();
-    let loki = tesseract::Tesseract::new_with_oem(None, Some("eng"), tesseract::OcrEngineMode::Default).unwrap();
-    let captcha_string = loki.set_image_from_mem(&image_vector).unwrap().get_text().unwrap();
-    println!("captcha : {}", captcha_string);
-    // std::fs::remove_file(&filename).unwrap();
+    let mut captcha_string: String = "".to_owned();
+    while captcha_string.len() != 6 {
+        let r = Request::builder()
+            .method(Method::POST)
+            .uri("https://erp.mitwpu.edu.in/AdminLogin.aspx/funGenerateCaptcha")
+            .header("Content-Type", "application/json; charset=utf-8")
+            .header("Content-Length", "0")
+            .body(Body::empty())
+            .unwrap();
+        let resp = client.request(r).await?;
+        let data = body::to_bytes(resp).await?;
+        let captcha_data: CaptchaData = serde_json::from_slice(&data).unwrap();
+        let image_vector = base64::decode(captcha_data.d).unwrap();
+        // let filename = format!("{}.png", Uuid::new_v4().to_string());
+        // std::fs::write(&filename, &image_vector).unwrap();
+        let loki = tesseract::Tesseract::new_with_oem(None, Some("eng"), tesseract::OcrEngineMode::Default).unwrap();
+        captcha_string = loki.set_image_from_mem(&image_vector).unwrap().set_variable("tessedit_char_whitelist", "abcdef1234567890").unwrap().get_text().unwrap().trim().to_owned();
+        println!("captcha : {}", captcha_string);
+        // std::fs::remove_file(&filename).unwrap();
+    }
+    
     Ok(())
 }
 
